5a7d64538b1f33cc38921eed2c4a52fca9a26ff5
ahangchen <1418659400@qq.com>
2016-06-02 14:41:18 +0800

    fix db name

--- WeMeet/settings.py
+++ WeMeet/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
+        'NAME': 'wemeet',
         'USER': 'hjf',
         'PASSWORD': 'Aa123456789',
         'HOST': 'localhost',
