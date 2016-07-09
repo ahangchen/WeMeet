7b6b2bd93000268d8c54ac7a0ab1ec6536891d44
ahangchen <1418659400@qq.com>
2016-07-10 00:02:58 +0800

    remove test code

--- team/ctrl/register.py
+++ team/ctrl/register.py
@@ -3,7 +3,7 @@ from io import BytesIO
 
 from django.http import HttpResponse
 
-from student.models import StuInfo
+# from student.models import StuInfo
 from team.util.image import create_validate_code
 
 
@@ -13,7 +13,7 @@ def validate(request):
     img, validate_code = create_validate_code()
     img.save(mstream, "GIF")
     request.session['code'] = validate_code
-    StuInfo().save()
-    StuInfo.objects.all()
+    # StuInfo().save()
+    # StuInfo.objects.all()
     img_bytes = base64.b64encode(mstream.getvalue()).decode()
     return HttpResponse(img_bytes)
