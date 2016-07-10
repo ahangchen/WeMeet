b1454dc61eab96dd0af734d025dcf5df0a6265c5
huang <961437466@qq.com>
2016-07-10 21:04:56 +0800

    fix_useless_files

--- student/utility/bussiness_logic.py
+++
@@ -1,6 +0,0 @@
-import re
-
-
-def get_file_type(file_name):
-    file_type = re.split('\.', file_name)[-1]
-    return file_type
--- student/utility/data_access.py
+++
@@ -1,9 +0,0 @@
-from student.utility.tag import NO_INPUT
-
-
-def value(previous_value, input_value):
-    """if not input value, then return the previous value"""
-    if input_value == NO_INPUT:
-        return previous_value
-    else:
-        return input_value
