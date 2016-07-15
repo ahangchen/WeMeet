dc8761d8cd09094c3aab905f92d5f2ac6fcc59aa
cwh <cweihang@foxmail.com>
2016-07-15 18:47:43 +0800

    util to dump dict to json with err code

--- student/utility/json_helper.py
+++ student/utility/json_helper.py
@@ -3,3 +3,7 @@ import json
 
 def dumps(json_ctx):
     return json.dumps(json_ctx, ensure_ascii=False)
+
+
+def dict2err_json(err_code, res_dict):
+    return "{'err': %d, 'res': '%s'}" % (err_code, dumps(res_dict))
