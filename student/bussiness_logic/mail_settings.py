12adef417766163516650f7d08e98d27d5c76177
huang <961437466@qq.com>
2016-07-15 23:21:03 +0800

    move mail settings

---
+++ student/bussiness_logic/mail_settings.py
@@ -0,0 +1,8 @@
+
+#邮件配置
+EMAIL_HOST = 'smtp.sina.com'                    # SMTP地址
+EMAIL_PORT = 25                                 # SMTP端口
+EMAIL_HOST_USER = 'm18826076291@sina.com'       # 暂时的邮箱
+EMAIL_HOST_PASSWORD = 'a123456789'              # 邮箱密码
+# EMAIL_SUBJECT_PREFIX = u'[WeMeet]'              # 为邮件Subject-line前缀,默认是'[django]'
+EMAIL_USE_TLS = False                           # 与SMTP服务器通信时，是否启动TLS链接(安全链接)。默认是false
