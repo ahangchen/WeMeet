caabb8b8a5334c91a69cbf404245f2c8fb7c5d5f
huang <961437466@qq.com>
2016-07-16 12:59:20 +0800

    remove decrypt

--- student/bussiness_logic/reset_mail.py
+++ student/bussiness_logic/reset_mail.py
@@ -1,8 +1,8 @@
 from student.utility.encrypt_decrypt import encrypt
 
 
-def get_content(account):
-    content = '这是邮件内容'+'student/reset/'+encrypt(account)  # TODO(hjf): 修改内容和URL
+def get_content(para):
+    content = '这是邮件内容'+'student/reset/'+para  # TODO(hjf): 修改内容和URL
     return content
 
 
--- student/data_access/account.py
+++ student/data_access/account.py
@@ -3,13 +3,12 @@ from student.models import StuInfo, StuAccount
 from student.data_access import stu_info
 from student.data_access.tag import ERR_INSERT_DB, OK_INSERT, ERR_DELETE_NOTEXIST, \
                                         OK_UPDATE, ERR_UPDATE_DB, ERR_UPDATE_NOTEXIST,\
-                                        ERR_SELECT_DB, ERR_SELECT_NOTEXIST
+                                        ERR_SELECT_DB, ERR_SELECT_NOTEXIST,\
+                                        OK_DELETE, ERR_DELETE_DB
 
 from student.utility.value_update import value, NO_INPUT
 from student.utility.date_helper import pre_date
 from student.utility.logger import logger
-
-
 
 DEFAULT_CIPHERTEXT = '032164879231378144'
 
@@ -53,21 +52,53 @@ def update(account, pwd=NO_INPUT, is_activated=NO_INPUT, ciphertext=NO_INPUT, re
         return ERR_UPDATE_DB
 
 
-def select(account):
+def select(account=NO_INPUT, ciphertext=NO_INPUT):
     """
     成功：返回查询的账号
     失败：返回ERR_SELECT_NOTEXIST
            或ERR_SELECT
     """
     try:
-        select_acnt = StuAccount.objects.all().get(account=account)
-        return select_acnt
+        if account != NO_INPUT and ciphertext == NO_INPUT:
+            select_acnt = StuAccount.objects.all().get(account=account)
+            return select_acnt
+
+        elif account == NO_INPUT and ciphertext != NO_INPUT:
+            select_acnt = StuAccount.objects.all().get(ciphertext=ciphertext)
+            return select_acnt
+
+        elif account != NO_INPUT and ciphertext != NO_INPUT:
+            select_acnt = StuAccount.objects.all().get(account=account, ciphertext=ciphertext)
+            return select_acnt
+
+        return
+
     except StuAccount.DoesNotExist:
         return ERR_SELECT_NOTEXIST
     except:
         logger.error('数据库异常导致账号查询失败')
         return ERR_SELECT_DB
 
+
+def delete(account):
+    try:
+        delete_acnt = StuInfo.objects.all().get(account=account)  # 抛出MultipleObjectsReturned或DoesNotExist
+        delete_acnt.delete()  # 不抛出异常
+        return OK_DELETE
+
+    except StuAccount.DoesNotExist:
+        logger.error('尝试删除不存在的账号')
+        return ERR_DELETE_DB
+
+    except StuInfo.MultipleObjectsReturned:
+        logger.info('数据库异常（存在重复记录）')
+        StuInfo.objects.all().filter(account=account).delete()  # 不抛异常
+        return OK_DELETE
+
+    # 数据库异常
+    except:
+        logger.error('数据库异常导致删除账号失败')
+        return ERR_DELETE_DB
 
 
 
