f6490c947f88bb5d0718be7071b5feb211c1a7fd
ahangchen <1418659400@qq.com>
2016-07-26 21:18:56 +0800

    ajax cookie

--- team/static/js/test.js
+++ team/static/js/test.js
@@ -3,9 +3,10 @@
  */
 function loadValidCode(id) {
     $.ajax({
-            // url: "http://110.64.69.66:8081/team/valid_code",
-            url: "http://127.0.0.1:8000/team/valid_code",
-            processData: false
+            url: "http://110.64.69.66:8081/team/valid_code",
+            // url: "http://127.0.0.1:8000/team/valid_code",
+            processData: false,
+            xhrFields: {withCredentials: true}
         }).always(function (data) {
                 console.log('test');
                 $(id).attr('src', "data:image/gif;base64," + data);
@@ -23,4 +24,25 @@ $(document).ready(function () {
     });
     loadValidCode('#check');
     loadValidCode('#check2');
+    $('#reg_btn').click(function () {
+        $.ajax({
+            type: "post",
+            url: "http://110.64.69.66:8081/team/register/",
+            dataType: "json",
+            data: {
+                "mail": $('#r_mail').val(),
+                "pwd": $('#r_pwd').val(),
+                "inv": $('#r_inv').val(),
+                "code": $('#r_code').val()
+            },
+            xhrFields: {withCredentials: true},
+            headers: {
+                "Access-Control-Allow-Origin":"*"
+            }
+
+        }).always(function (data) {
+            console.log(data);
+        });
+
+    })
 });
