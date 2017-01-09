335932370febefa13b547621da33e7e97901c0bd
ahangchen <1418659400@qq.com>
2017-01-09 14:28:13 +0800

    邮件内容完善

--- student/ctrl/verify_mail.py
+++ student/ctrl/verify_mail.py
@@ -2,7 +2,7 @@ from student.util.encrypt_decrypt import encrypt
 
 
 def get_content(para):
-    content = '这是邮件内容'+' http://wemeet.tech:8081/student/activate/'+para  # TODO(hjf): 修改内容和URL
+    content = '点击链接完成激活： '+' http://wemeet.tech:8081/student/activate/'+para  # TODO(hjf): 修改内容和URL
     return content
 
 
